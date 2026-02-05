5ddd6df14ab219bf068d324d8ea025ca2665d37e
TaffyNo1cute <lihubo074@gmail.com>
2026-02-05 20:53:28 +0800

    删除系统缓存文件

--- .docusaurus/client-modules.js
+++
@@ -1,6 +0,0 @@
-export default [
-  require("C:\\Users\\AOC\\Desktop\\fi\\University-Guide\\node_modules\\infima\\dist\\css\\default\\default.css"),
-  require("C:\\Users\\AOC\\Desktop\\fi\\University-Guide\\node_modules\\@docusaurus\\theme-classic\\lib\\prism-include-languages"),
-  require("C:\\Users\\AOC\\Desktop\\fi\\University-Guide\\node_modules\\@docusaurus\\theme-classic\\lib\\nprogress"),
-  require("C:\\Users\\AOC\\Desktop\\fi\\University-Guide\\src\\css\\custom.css"),
-];
