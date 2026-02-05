210c1f8587878b17a59c90feaa608b26e3c47653
moyuhao <myhnone@163.com>
2026-02-05 20:37:19 +0800

    一些提交

--- .docusaurus/client-modules.js
+++ .docusaurus/client-modules.js
@@ -1,6 +1,6 @@
 export default [
-  require("C:\\Users\\AOC\\Desktop\\fi\\University-Guide\\node_modules\\infima\\dist\\css\\default\\default.css"),
-  require("C:\\Users\\AOC\\Desktop\\fi\\University-Guide\\node_modules\\@docusaurus\\theme-classic\\lib\\prism-include-languages"),
-  require("C:\\Users\\AOC\\Desktop\\fi\\University-Guide\\node_modules\\@docusaurus\\theme-classic\\lib\\nprogress"),
-  require("C:\\Users\\AOC\\Desktop\\fi\\University-Guide\\src\\css\\custom.css"),
+  require("F:\\ALLproject\\University-Guide\\node_modules\\infima\\dist\\css\\default\\default.css"),
+  require("F:\\ALLproject\\University-Guide\\node_modules\\@docusaurus\\theme-classic\\lib\\prism-include-languages"),
+  require("F:\\ALLproject\\University-Guide\\node_modules\\@docusaurus\\theme-classic\\lib\\nprogress"),
+  require("F:\\ALLproject\\University-Guide\\src\\css\\custom.css"),
 ];
