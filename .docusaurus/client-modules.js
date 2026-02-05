4256fd7d934ae90012d365795bfd7cd26f35589b
moyuhao <myhnone@163.com>
2026-02-05 20:44:26 +0800

    chore: 从版本控制中移除 .docusaurus 缓存文件夹

--- .docusaurus/client-modules.js
+++
@@ -1,6 +0,0 @@
-export default [
-  require("F:\\ALLproject\\University-Guide\\node_modules\\infima\\dist\\css\\default\\default.css"),
-  require("F:\\ALLproject\\University-Guide\\node_modules\\@docusaurus\\theme-classic\\lib\\prism-include-languages"),
-  require("F:\\ALLproject\\University-Guide\\node_modules\\@docusaurus\\theme-classic\\lib\\nprogress"),
-  require("F:\\ALLproject\\University-Guide\\src\\css\\custom.css"),
-];
